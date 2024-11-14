b918f934fcc616c0a3de0d2aaa540fcaf01c7be2
JefersonRodrigues21 <dev.geekjs@gmail.com>
2024-11-14 15:57:09 -0300

    alteração de pastas

--- desafio1.js
+++
@@ -1,9 +0,0 @@
-
-let valorCombustível = 5.79;
-let gastoMedioCombustivelKm = 0.120;
-let distancia_Km_viagem = 1580;
-
-const combustivelConsumido = distancia_Km_viagem * gastoMedioCombustivelKm;
-const valorViagem = combustivelConsumido * valorCombustível;
-
-console.log('O valor a gastar pela viagem será de ' + valorViagem.toFixed(2));
