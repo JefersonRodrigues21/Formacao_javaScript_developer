6e80b0134e94677611efdac1771dedcbf06a56e1
JefersonRodrigues21 <dev.geekjs@gmail.com>
2024-11-14 16:04:59 -0300

    arquivo txt excluido

--- variaveis_E_operadores/desafio1.js
+++ variaveis_E_operadores/desafio1.js
@@ -1,3 +1,11 @@
+// Faça um program para calcular o valor de uma viagem.
+
+// Voce terá 3 variáveis. Sendo elas:
+// 1 - Preço do combustível;
+// 2 - Gasto médio de combustível do carro por km;
+// 3 - Distancia em km da viagem.
+
+// Imprima no console o valor que será gasto para realizar esta viagem.
 
 let valorCombustível = 5.79;
 let gastoMedioCombustivelKm = 0.120;
