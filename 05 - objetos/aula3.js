cf7fb507d5861a3914f1431ebee16391f7a78672
JefersonRodrigues21 <dev.geekjs@gmail.com>
2024-12-06 11:22:27 -0300

    Recebendo objetos

--- 05 - objetos/aula3.js
+++ 05 - objetos/aula3.js
@@ -14,8 +14,21 @@ class Pessoa {
     }
 }
 
-const vitor = new Pessoa('Vitor Guerra', 25);
-const renan = new Pessoa('Renan Paula', 30);
-const jeferson = new Pessoa('Jeferson Rodrigues', 37);
+// const jeferson = new Pessoa('Jeferson Rodrigues', 37);
 
-console.log(jeferson, renan, vitor);
+// console.log(jeferson);
+
+function compararPessoas(p1, p2) {
+    if (p1.idade > p2.idade) {
+        console.log(`${p1.nome} é mais velho(a) que ${p2.nome}.`);
+    } else if (p1.idade < p2.idade){
+    console.log(`${p2.nome} é mais velho(a) que ${p1.nome}.`);
+    } else {
+    console.log(`${p1.nome} e ${p2.nome} tem a mesma idade.`);
+    }
+}
+
+const vitor = new Pessoa('Vitor', 25);
+const renan = new Pessoa('Renan', 30);
+
+compararPessoas(vitor, renan);
