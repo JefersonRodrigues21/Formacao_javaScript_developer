c84984885f1c0ab2658d6ec32c975f931f4646a0
JefersonRodrigues21 <dev.geekjs@gmail.com>
2024-12-06 10:27:47 -0300

    Instâncias com Construtor

--- 05 - objetos/aula3.js
+++ 05 - objetos/aula3.js
@@ -1,19 +1,21 @@
 class Pessoa {
     nome;
     idade;
+    anoDeNascimento;
+
+    constructor(nome, idade){
+        this.nome= nome;
+        this.idade= idade;
+        this.anoDeNascimento = 2024 - idade;
+    }
 
     descrever() {
         console.log(`Meu nome é ${this.nome} e minha idade é ${this.idade}`);
     }
 }
 
-const vitor = new Pessoa();
-vitor.nome = 'Vitor J Guerra';
-vitor.idade = 25;
+const vitor = new Pessoa('Vitor Guerra', 25);
+const renan = new Pessoa('Renan Paula', 30);
+const jeferson = new Pessoa('Jeferson Rodrigues', 37);
 
-const renan = new Pessoa();
-renan.nome = 'Renan J Paula';
-renan.idade = 30;
-
-vitor.descrever();
-renan.descrever();
+console.log(jeferson, renan, vitor);
