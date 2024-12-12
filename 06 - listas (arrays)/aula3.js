1ccb3321f257ece8343ddc27fc173f9642b34af1
JefersonRodrigues21 <dev.geekjs@gmail.com>
2024-12-12 14:33:53 -0300

    executando for em uma string

---
+++ 06 - listas (arrays)/aula3.js
@@ -0,0 +1,17 @@
+
+// for (let i = 0; i < 10; i ++) { /*i = 0, enquanto i < 10; faça/execute i + 1 */
+//        console.log(i);
+// }
+
+const nome = 'JEFERSON RODRIGUES';
+
+for (let i = 0; i < nome.length; i++) {
+    const letra = nome[i];
+    console.log(letra);
+    
+}
+
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+}
