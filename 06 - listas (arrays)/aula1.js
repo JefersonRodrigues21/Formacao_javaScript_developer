4960609836ae847450372a9e58d6de9ad4485abc
JefersonRodrigues21 <dev.geekjs@gmail.com>
2024-12-12 10:12:52 -0300

    formas de inserir na lista

--- 06 - listas (arrays)/aula1.js
+++ 06 - listas (arrays)/aula1.js
@@ -1,3 +1,8 @@
-const alunos = ['João', 'Vitor', 'Marina'];
+/* {} objeto, [] listas */
+
+const alunos = ['João', 'Vitor', 'Joana'];
+
+alunos.push('Marcela'); /*forma de inserir */
+alunos[4] = 'Matheus'; /*forma de inserir */
 
 console.log(alunos);
