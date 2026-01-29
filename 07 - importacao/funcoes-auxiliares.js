f7244603ab4e26de104b4021d895866e2d45c23a
JefersonRodrigues21 <dev.geekjs@gmail.com>
2026-01-29 16:47:50 -0300

    Exercício

--- 07 - importacao/funcoes-auxiliares.js
+++ 07 - importacao/funcoes-auxiliares.js
@@ -1,7 +1,11 @@
 
+const entradas = [5, 50, 10, 98, 23]; // Array simulando entradas de dados
+let i = 0; // Índice para controlar a posição no array de entradas
 
-function gets() { //
-    return 10;
+function gets() { // Simula a função gets, retornando um valor fixo
+    const valor = entradas[i]; // Pega o valor na posição atual do índice
+    i++;
+    return valor; // Retorna o valor e incrementa o índice
 }
 
 function print(texto) { // Recebe um texto como parâmetro
--- 07 - importacao/main.js
+++ 07 - importacao/main.js
@@ -2,5 +2,27 @@
 
 const {gets, print} = require('./funcoes-auxiliares.js'); // Importação das funções auxiliares
 
+/**
+ Uma sala contém 5 alunos e para cada aluno foi sorteado um número de 1 a 100.
+ Faça um programa que receba os 5 números sorteados para os alunos e mostre e mostre
+ o maior número sorteado.
+
+ Dados de entrada:
+    5
+    50
+    10
+    98
+    23
+
+    Dados de saída:
+    98
+ */
+
 
 print(gets()); // Chamada da função print, passando como argumento o valor retornado pela função gets
+print(gets());
+print(gets());
+print(gets());
+print(gets());
+print(gets());
+
